be082eaf535eb0d56e728fd90f05b32d803ac5f9
Manish Panchal <panchalmanish2208@gmail.com>
2024-06-21 20:03:37 +0530

    update: Removed credentials from firebase.config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 //web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3gzPPUTa5vkxLkJ9bIMdv1IxD1oIWStw",
-  authDomain: "house-marketplace-61a8b.firebaseapp.com",
-  projectId: "house-marketplace-61a8b",
-  storageBucket: "house-marketplace-61a8b.appspot.com",
-  messagingSenderId: "814408089630",
-  appId: "1:814408089630:web:879121eed8e9ca4d60ae0f",
-  measurementId: "G-0FKV1PN4CE"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 
